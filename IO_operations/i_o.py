a01db157edfc32025aeda09fd1e0cb149bb833ad
Tushar <maliktushar749@gmail.com>
2026-05-16 15:51:12 +0530

    updated io file

--- IO_operations/i_o.py
+++ IO_operations/i_o.py
@@ -5,19 +5,19 @@ import pandas as pd
 class DataLoader:
     @staticmethod
     def load_data(file_path):
-        if not os.path.exists(file_path):
+        if isinstance(file_path, (str, bytes, os.PathLike)) and not os.path.exists(file_path):
             raise FileNotFoundError(f"The file {file_path} does not exist.")
-    
+
         try:
             data = pd.read_csv(file_path)
             return data
         except Exception as e:
             print(f"An error occurred while loading the data: {e}")
-        return None
-    
+            return None
+
     @staticmethod
-    def close_file(file:pd.DataFrame):
+    def close_file(file: pd.DataFrame):
         try:
-             del file
+            del file
         except Exception as e:
             print(f"An error occurred while closing the file: {e}")
